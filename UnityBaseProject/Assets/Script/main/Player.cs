330cab24e3e5c5bdff2cb8b164dbd6db5686ce88
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2017-10-26 18:43:14 +0900

    銛途中、マルチインプット他追加

--- UnityBaseProject/Assets/Script/main/Player.cs
+++ UnityBaseProject/Assets/Script/main/Player.cs
@@ -2,21 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testScript : ObjectBase {
+public class Player : ObjectBase {
+	[SerializeField]
+	private HookShot hookShot;
 
 	// Use this for initialization
-	void Start () {
-		m_OrderNumber = 3;
+	void Start() {
+		m_OrderNumber = 0;
 		ObjectManager.Instance.RegistrationList(this, m_OrderNumber);
 
 	}
 
 	public override void Execute(float deltaTime) {
-		//transform.position += new Vector3(1,0,0);
-		//      if(Input.anyKeyDown) {
-		//          CSParticleManager.Instance.Play("test");
-		//      }
-		transform.Rotate(new Vector3(0,0,-1000 * deltaTime));
+		if(MultiInput.Instance.GetTriggerButton(MultiInput.CONTROLLER_BUTTON.CIRCLE)) {
+			hookShot.Shot(transform);
+		}
 	}
 
 	public override void LateExecute(float deltaTime) {

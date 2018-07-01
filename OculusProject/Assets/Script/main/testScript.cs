bcc0f8879ebb7fcee8da58acc3fbddbba5b1cd68
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2018-07-01 23:51:19 +0900

    オキュラス用プロジェクト作成

---
+++ OculusProject/Assets/Script/main/testScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : ObjectBase {
+
+	// Use this for initialization
+	void Start () {
+		m_OrderNumber = 3;
+		ObjectManager.Instance.RegistrationList(this, m_OrderNumber);
+
+	}
+
+	public override void Execute(float deltaTime) {
+		//transform.position += new Vector3(1,0,0);
+		//      if(Input.anyKeyDown) {
+		//          CSParticleManager.Instance.Play("test");
+		//      }
+		transform.Rotate(new Vector3(0,0,-1000 * deltaTime));
+	}
+
+	public override void LateExecute(float deltaTime) {
+
+	}
+}

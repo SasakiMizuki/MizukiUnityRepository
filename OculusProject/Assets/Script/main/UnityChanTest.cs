c5db602f486b60822fd27f2f75f17bcf9062b68d
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2018-08-18 00:13:45 +0900

    oculus

---
+++ OculusProject/Assets/Script/main/UnityChanTest.cs
@@ -0,0 +1,48 @@
+﻿//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+//	UnityChanTest.cs
+//	
+//	作成者:
+//==================================================
+//	概要
+//	
+//  
+//	
+//==================================================
+//	作成日：yyyy/mm/dd
+//	
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanTest : ObjectBase {
+    // メンバ
+    #region Member
+    [SerializeField]
+    Animator anim;
+	#endregion Member
+
+	// 定数
+	#region Constant
+	
+	#endregion Constant
+
+	// メソッド
+	#region Method
+	void Start () {
+		m_OrderNumber = 0;
+		ObjectManager.Instance.RegistrationList(this, m_OrderNumber);
+	}
+	
+	public override void Execute(float deltaTime) {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)) {
+            anim.SetTrigger("Rest_trigger");
+        }
+	}
+
+	public override void LateExecute(float deltaTime) {
+		
+	}
+	#endregion Method
+}
